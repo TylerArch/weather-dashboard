dd5887f818aab433db49d998685ad76f91a10a14
Tyler Arch <TylerArch@MacBook-Pro.local>
2022-09-28 20:16:22 -0500

    attempt to recall previous searches from local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,58 @@
 //DOM Elements
 let searchButton = document.getElementById('searchButton');
-let input = document.getElementById('formInput');
+let formInput = document.getElementById('formInput');
 let mainWeather = document.getElementById('apiHeader');
 let mainTemp = document.getElementById('temp');
 let mainWind = document.getElementById('wind');
 let mainHumidity = document.getElementById('humidity');
+let searchHistoryContainer = document.getElementById('container');
+var historyArr = [];
 
+//do I need moment?
 
 //Global Variables
 const weatherDays = [];
 let currentDay = null;
 
 
+//Function
+function showSearchHistory() {
+    searchHistoryContainer.innerHTML = "";
+    //Checking search history array for previous searches and creating the amount of buttons equal to the elements in the array
+    for (var i = historyArr.length -1; i >= 0; i--) {
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('history-btn');
 
-//Functions
+        btn.setAttribute('data-search', historyArr[i]);
+
+        btn.textContent = historyArr[i];
+        searchHistoryContainer.append(btn);
+    }
+}
+
+    //Function to update the search history in local storage
+function addToHistory(city) {
+        //If nothing was searched return
+    if(historyArr.indexOf(city) !== -1){
+            return;
+    }
+    //If there is search history display it
+    historyArr.push(city);
+    localStorage.setItem('search-history', JSON.stringify(historyArr));
+    showSearchHistory();
+}
+
+//Function to get the local storage items
+function getSearches() {
+    var savedHistory = localStorage.getItem('search-history');
+    if (savedHistory) {
+        historyArr = JSON.parse(savedHistory)
+    } showSearchHistory();
+}
+
 //Function to get API
-function getApi(event) {
+function getApi(city) {
     var city = input.value
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=188a51d5f423814817d69f7e3e5961b3`;
 
@@ -28,10 +65,26 @@ function getApi(event) {
 
         console.log(data);
         console.log(data.city.name);
-})
+    })
 }
 
-//function to push the data to local storage
+function searchButtonClick(event) {
+    if (!formInput.value){
+    return;
+    }
+    event.preventDefault();
+    var city = formInput.value.trim();
+    getApi(city)
+    formInput.value = '';
+}
+
+getSearches();
+formInput.addEventListener('submit', searchButtonClick);
+
+
+//Function to push 5 day forcast to h3
+
+
 
 
 //From Gary????
@@ -48,4 +101,4 @@ function getApi(event) {
     
    
 //Event Listener
-searchButton.addEventListener('click', getApi)
+// searchButton.addEventListener('click', getApi)
